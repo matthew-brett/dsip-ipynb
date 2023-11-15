93ccc77275d11052caf471276072b827fd7c1c86
Matthew Brett <matthew.brett@gmail.com>
2023-11-15 15:34:45 +0000

    Expand configure_shell

--- configure_shell.py
+++ configure_shell.py
@@ -16,7 +16,10 @@ USER_PATH = Path('~').expanduser()
 def get_parser():
     parser = ArgumentParser(description=__doc__,  # Usage from docstring
                             formatter_class=RawDescriptionHelpFormatter)
+    parser.add_argument('-x', '--allow-existing', action='store_true',
+                        help='Force config write even if dir already on PATH')
     return parser
+
 
 
 def get_paths():
@@ -30,26 +33,46 @@ def get_mac_shell():
     return re.match(r'UserShell: (/\w+)+', shell_info).groups()[-1][1:]
 
 
-def get_mac_configfile():
-    shell = get_mac_shell()
+def get_unix_shell():
+    from getpass import getuser
+    shell_info = check_output(f'getent passwd {getuser()}',
+                              shell=True, text=True).strip()
+    return shell_info.split(':')[-1].split('/')[-1]
+
+
+def get_posix_configfile():
+    is_mac = os.name == 'darwin'
+    shell = get_mac_shell() if is_mac else get_unix_shell()
     if shell == 'bash':
-        return USER_PATH / '.bash_profile'
+        return USER_PATH / ('.bash_profile' if is_mac else '.bashrc')
     elif shell == 'zsh':
         return USER_PATH / '.zshrc'
     else:
-        raise RuntimeError(f'Unpexpected shell {shell}')
+        raise RuntimeError(f'Unexpected shell {shell}')
 
 
-def echo_windows_recipe(site_path):
-    print('''\
-Run the following commands in an Administrator Powershell window:
+def set_windows_path(site_path):
+    res = check_output(f'setx PATH "%PATH%;{site_path}',
+                       shell=True,
+                       text=True).split()
+    if not res.startswith('SUCCESS:'):
+        raise RuntimeError(f'SETX failed: {res}')
 
-$site_path=python3 -c 'import os,sysconfig;print(sysconfig.get_path("scripts",f"{os.name}_user"))'
-$user_path=[Environment]::GetEnvironmentVariable("PATH", "User")
-[Environment]::SetEnvironmentVariable("PATH", "$user_path;$site_path", [System.EnvironmentVariableTarget]::User)
 
-Then close all your Powershell tabs / windows and open a new Powershell window.
-''')
+def set_path_config(site_path):
+    if os.name == 'nt':
+        set_windows_path(site_path)
+        return
+    sp_rel = site_path.relative_to(USER_PATH)
+    out_text = f'''
+# Inserted by configure_shell
+# Add Python --user script directory to PATH
+export PATH="$PATH:${{HOME}}/{str(sp_rel)}"
+'''
+    config_path = get_posix_configfile()
+    config_text = config_path.read_text()
+    if not out_text in config_text:
+        config_path.write_text(f'{config_text}\n{out_text}')
 
 
 def main():
@@ -57,24 +80,12 @@ def main():
     args = parser.parse_args()
     site_path_str = sysconfig.get_path("scripts",f"{os.name}_user")
     site_path = Path(site_path_str).resolve()
-    if site_path in get_paths():
+    if not args.allow_existing and site_path in get_paths():
         print(f'{site_path} already appears to be on your PATH')
         sys.exit(1)
-    if os.name == 'nt':
-        echo_windows_recipe(site_path)
-        sys.exit(0)
-    sp_rel = site_path.relative_to(USER_PATH)
-    out_text = f'''
-# Inserted by configure_shell.py
-# Add Python --user script directory to PATH
-export PATH="$PATH:${{HOME}}/{str(sp_rel)}"
-'''
-    if sys.platform == 'darwin':
-        config_path = get_mac_configfile()
-        config_text = config_path.read_text()
-        config_path.write_text(f'{config_text}\n{out_text}')
-        sys.exit(0)
-    raise RuntimeError(f'Unexpected platform {sys.platform}')
+    set_path_config(site_path)
+    print("""\
+Now close all terminals and start a new terminal to load new config""")
 
 
 if __name__ == '__main__':
